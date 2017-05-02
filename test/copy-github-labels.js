f5d158f576f24c4bb2015b9ef98b2ca6e21ffbf0
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2017-05-02 00:32:25 -0700

    style: fix up test style

--- test/copy-github-labels.js
+++ test/copy-github-labels.js
@@ -1,36 +1,29 @@
-var CopyGitHubLabels = require('../');
-var chai = require('chai');
-var expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
+const CopyGitHubLabels = require('../')
 
-describe('CopyGitHubLabels', function(){
+describe('CopyGitHubLabels', function() {
+  it('should be a function', function() {
+    expect(CopyGitHubLabels).to.be.a('function')
+  })
+})
 
-	it('should be a function', function(){
-		expect(CopyGitHubLabels).to.be.a('function');
-	});
+describe('copyGitHubLabels', function() {
+  var copyGitHubLabels = CopyGitHubLabels()
 
-});
+  it('should be an object', function() {
+    expect(copyGitHubLabels).to.be.a('object')
+  })
 
-describe('copyGitHubLabels', function(){
+  describe('#authenticate', function() {
+    it('should be a function', function() {
+      expect(copyGitHubLabels.authenticate).to.be.a('function')
+    })
+  })
 
-	var copyGitHubLabels = new CopyGitHubLabels();
-
-	it('should be an object', function(){
-		expect(copyGitHubLabels).to.be.a('object');
-	});
-
-	describe('#authenticate', function(){
-
-		it('should be a function', function(){
-			expect(copyGitHubLabels.authenticate).to.be.a('function');
-		});
-
-	});
-
-	describe('#copy', function(){
-
-		it('should be a function', function(){
-			expect(copyGitHubLabels.copy).to.be.a('function');
-		});
-
-	});
-});
+  describe('#copy', function() {
+    it('should be a function', function() {
+      expect(copyGitHubLabels.copy).to.be.a('function')
+    })
+  })
+})
